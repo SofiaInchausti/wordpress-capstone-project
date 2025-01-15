6b14885936009ad3edcc71c64dbde7e9d61b8599
sofiainchausti <sofiabeleninchausti@gmail.com>
2025-01-15 11:42:00 -0300

    2.2 Custom Post Types

--- theme/functions.php
+++ theme/functions.php
@@ -4,3 +4,15 @@ function my_theme_enqueue_styles() {
 ());
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function create_books_cpt() {
+    register_post_type('books', [
+        'labels' => [
+            'name' => 'Books',
+            'singular_name' => 'Book',
+        ],
+        'public' => true,
+        'supports' => ['title', 'editor', 'thumbnail'],
+        'menu_icon' => 'dashicons-book',
+]); }
+add_action('init', 'create_books_cpt');
