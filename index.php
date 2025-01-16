cb3d429214f2681efd97ca52bcec1cc0282d0673
sofiainchausti <sofiabeleninchausti@gmail.com>
2025-01-16 15:58:45 -0300

    02-themes/advanced-theme/react/

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-<div id="root">
-    <Books/>
-</div>
-<?php get_footer(); ?>
--- theme/functions.php
+++ theme/functions.php
@@ -13,5 +13,35 @@ function create_books_cpt() {
         'public' => true,
         'supports' => ['title', 'editor', 'thumbnail'],
         'menu_icon' => 'dashicons-book',
+        'has_archive' => true, 
+        'rewrite' => ['slug' => 'books'],
+        'show_in_rest' =>  true
 ]); }
 add_action('init', 'create_books_cpt');
+
+function ryp_enqueue_scripts() {
+    wp_enqueue_style('react-style',  get_template_directory_uri() . '/build/static/css/main.css', // Update path as necessary
+    null,
+    true
+);
+    wp_enqueue_script('react-script', get_template_directory_uri() . '/build/static/js/main.js', // Update path as necessary
+    ['wp-element'],
+    null,
+    true
+);
+}
+add_action('wp_enqueue_scripts', 'ryp_enqueue_scripts');
+
+function ryp_render_react_app() {
+    echo '<div id="root"></div>';
+}
+add_action('wp_footer', 'ryp_render_react_app');
+
+function add_type_attribute($tag, $handle, $src)
+{
+  if ($handle === 'react-script') {
+    $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
+  }
+  return $tag;
+}
+add_filter('script_loader_tag', 'add_type_attribute', 10, 3);
